6874c0b3dcc6734b7921aa6bb4c323ab6780cfb0
Orestes Roussos <roussos8bit@gmail.com>
2016-11-28 16:22:49 +0100

    minor logic error in arguments fixed

--- parse_interface_withouth_spark.py
+++ parse_interface_withouth_spark.py
@@ -138,6 +138,7 @@ def linkEntities(entities):
 	return linked_entities
 
 def runProcedure(argv):
+	print argv
 	WARC_RECORD_ID = argv[0]
 	file_name = validateInput(argv[1])
 	output_name = argv[2]
@@ -210,7 +211,7 @@ def main(argv):
 
 if __name__ == "__main__":
 	argv = sys.argv
-	if len(argv)<2:
+	if len(argv)<3:
 		print "Provide the .warc.gz file as the first argument along with the script call.\nE.g.: python ~/parser.py sample.warc.gz"
 	else:
 		main(sys.argv[1:])
